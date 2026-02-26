19ef0c9f3eca59ff869b8777f2d289b9834b1434
kunchtler <leo.kulinski@gmail.com>
2026-02-26 19:34:36 +0100

    Opened the whole module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,9 @@
-module application {
+open module application {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
     requires java.desktop;
-
-    opens application to javafx.fxml;
 
     exports application;
 }
